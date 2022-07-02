e454d4b11b92c5373069912ac9ee38b734a306cf
kumibrr <isra.lora99@gmail.com>
2022-07-02 16:27:22 +0200

    Added default layout

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import Winbar from "$lib/winbar.svelte";
+  import github from '$lib/assets/github.png'
+  import DesktopIcon from "$lib/desktop-icon.svelte";
+</script>
+<div class="desktop">
+  <DesktopIcon icon={github} iconAlt="github icon" name="Link to repo" href="https://github.com/kumibrr"></DesktopIcon>
+</div>
+<slot></slot>
+<Winbar></Winbar>
+<style>
+  :root {
+    background-color: #018281;
+  }
+  :global(body) {
+    margin: 0;
+  }
+  .desktop {
+    position: absolute;
+    margin: 10px;
+    width: 100%;
+    height: 95%;
+    z-index: -1;
+  }
+</style>
