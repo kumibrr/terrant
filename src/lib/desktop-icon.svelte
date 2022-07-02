4c9fae5f62a4f0b362051cf6e84a904883b6a339
kumibrr <isra.lora99@gmail.com>
2022-07-02 16:27:06 +0200

    (feat) created base components for desktop

---
+++ src/lib/desktop-icon.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  export let icon: string;
+  export let iconAlt: string;
+  export let name: string;
+  export let href: string;
+</script>
+
+<a {href} target="_blank" rel="noopener noreferrer">
+  <img src={icon} alt={iconAlt}>
+  <p>{name}</p>
+</a>
+
+<style>
+  a {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    max-width: 70px;
+    color: aliceblue;
+    text-decoration: none;
+  }
+  a:focus {
+    outline: 1px dotted aliceblue;
+    text-decoration: dashed;
+  }
+  img {
+    max-width: 50px;
+  }
+  p {
+    margin: 0;
+    font-size: 13px;
+  }
+</style>
---
+++ src/lib/time.svelte
@@ -0,0 +1,6 @@
+<p>23:30</p>
+<style>
+  p {
+    margin: 0
+  }
+</style>
