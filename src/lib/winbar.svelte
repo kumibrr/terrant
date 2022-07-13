e39e438b49daea70cc9acb7bae75c235b2884122
kumibrr <isra.lora99@gmail.com>
2022-07-13 20:12:15 +0200

    (winbar) Added transition to menubar

--- src/lib/winbar.svelte
+++ src/lib/winbar.svelte
@@ -1,11 +1,25 @@
 <script lang="ts">
 	import midu from '$lib/assets/midu.png';
 	import Time from './time.svelte';
+	import Menu from './menu.svelte';
+	import { slide } from 'svelte/transition';
+
+	let showMenu = false;
 </script>
 
+{#if showMenu}
+	<div class="menu-bar" transition:slide={{ duration: 100 }}>
+		<Menu />
+	</div>
+{/if}
 <footer>
 	<div>
-		<button class="start">
+		<button
+			class="start"
+			on:click={() => {
+				showMenu = !showMenu;
+			}}
+		>
 			<img src={midu} alt="midu logo" />
 			Start
 		</button>
@@ -25,11 +39,7 @@
 		justify-content: space-between;
 		width: 100%;
 		bottom: 0;
-		border: none;
-		border-radius: 0;
 		box-sizing: border-box;
-		min-height: 23px;
-		min-width: 75px;
 		padding: 4px 4px;
 		background: silver;
 		box-shadow: inset -1px -1px #0a0a0a, inset 1px 1px #fff, inset -2px -2px grey,
@@ -60,5 +70,10 @@
 	img {
 		width: 20px;
 		margin-right: 10px;
+	}
+
+	.menu-bar {
+		position: absolute;
+		bottom: 32px;
 	}
 </style>
