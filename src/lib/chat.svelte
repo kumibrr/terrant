edffb06bf76b2b7dd927da22da4efd50474d0465
kumibrr <isra.lora99@gmail.com>
2022-07-19 23:37:30 +0200

    (chat) Added basic styles to chat

--- src/lib/chat.svelte
+++ src/lib/chat.svelte
@@ -1,38 +1,74 @@
 <script lang="ts">
-	import { draggable } from '@neodrag/svelte';
-	import antenna from '$lib/assets/antenna.png';
+	import Window from './window.svelte';
 	export let name: string;
+	export let icon: string;
 </script>
 
-<div class="window" use:draggable={{ handle: '.title-bar' }}>
-	<div class="title-bar">
-		<div class="title-bar-text">
-			<img src={antenna} alt="chat logo" class="title-bar-icon" draggable="false" />
-			{name}
-		</div>
-		<div class="title-bar-controls">
-			<button aria-label="Minimize" />
-			<button aria-label="Maximize" />
-			<button aria-label="Close" />
+<Window {icon} {name} backgroundColor="orange">
+	<div class="root">
+		<header>
+			//! TODO: Add wordart instead of text
+			<h1>chat</h1>
+			<h2>de terran't</h2>
+			<div class="actions">
+				<button><p>Create chat room</p></button>
+				<button><p>Join chat room</p></button>
+			</div>
+		</header>
+		<div class="body">
+			<p>Current room:</p>
+			<div />
+			<!-- <input type="text" /> -->
 		</div>
 	</div>
-	<div class="window-body"><p>Hola buenas</p></div>
-</div>
+</Window>
 
 <style>
-	.window {
-		position: absolute;
-		width: 300px;
-		user-select: none;
-		resize: both;
-		overflow: auto;
-		min-width: 20vw;
-		min-height: 10vh;
+	div.actions button {
+		background-color: yellow;
+		color: rgb(22, 203, 239);
+		border: none;
+		box-shadow: none;
+		text-transform: uppercase;
+		padding: 0;
+		padding-left: 2px;
+		padding-right: 2px;
+		outline: none;
+		font-weight: bold;
 	}
-	.title-bar-icon {
-		max-height: 12px;
+	div.actions button:active {
+		box-shadow: inset -1px -1px #75abc6, inset 1px 1px #b7a723, inset -2px -2px #a47419,
+			inset 2px 2px rgb(181, 118, 0);
 	}
-	.title-bar:active {
-		cursor: move;
+	p {
+		margin: 0;
+	}
+	h1,
+	h2 {
+		font-family: 'Courier New', Courier, monospace;
+		background-color: rgb(64, 64, 239);
+		color: rgb(22, 203, 239);
+		margin: 0;
+	}
+	h2 {
+		padding-left: 40px;
+	}
+	.body {
+		border: 2px solid black;
+		margin-top: 10px;
+		padding: 2px;
+		font-weight: bold;
+		display: flex;
+		flex-direction: column;
+		flex: 1;
+	}
+	.body div {
+		background-color: white;
+		flex: 1;
+	}
+	.root {
+		display: flex;
+		flex-direction: column;
+		flex-grow: 1;
 	}
 </style>
---
+++ src/lib/window.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import { draggable } from '@neodrag/svelte';
+	export let name: string;
+	export let icon: string;
+	export let backgroundColor: string;
+</script>
+
+<div
+	class="window"
+	use:draggable={{ handle: '.title-bar' }}
+	style={`background-color: ${backgroundColor};`}
+>
+	<div class="title-bar">
+		<div class="title-bar-text">
+			<img src={icon} alt="chat logo" class="title-bar-icon" draggable="false" />
+			{name}
+		</div>
+		<div class="title-bar-controls">
+			<button aria-label="Minimize" />
+			<button aria-label="Maximize" />
+			<button aria-label="Close" />
+		</div>
+	</div>
+	<div class="window-body">
+		<slot />
+	</div>
+</div>
+
+<style>
+	.window {
+		position: absolute;
+		user-select: none;
+		resize: both;
+		overflow: auto;
+		min-width: 350px;
+		min-height: 500px;
+		display: flex;
+		flex-direction: column;
+	}
+	.title-bar-icon {
+		max-height: 12px;
+	}
+	.title-bar:active {
+		cursor: move;
+	}
+	.window-body {
+		flex: 1;
+	}
+</style>
