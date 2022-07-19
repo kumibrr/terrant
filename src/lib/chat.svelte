dcfc573183b9b6ec4fd68dcd88d5216991d78b2d
kumibrr <isra.lora99@gmail.com>
2022-07-19 19:14:21 +0200

    (chat) Moved draggable behaviour to chatComponent. Added resizable behaviour

--- src/lib/chat.svelte
+++ src/lib/chat.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
+	import { draggable } from '@neodrag/svelte';
 	import antenna from '$lib/assets/antenna.png';
 	export let name: string;
 </script>
 
-<div class="window">
+<div class="window" use:draggable={{ handle: '.title-bar' }}>
 	<div class="title-bar">
 		<div class="title-bar-text">
-			<img src={antenna} alt="chat logo" class="title-bar-icon" />
+			<img src={antenna} alt="chat logo" class="title-bar-icon" draggable="false" />
 			{name}
 		</div>
 		<div class="title-bar-controls">
@@ -15,15 +16,23 @@
 			<button aria-label="Close" />
 		</div>
 	</div>
-	<div class="window-body">Hola buenas</div>
+	<div class="window-body"><p>Hola buenas</p></div>
 </div>
 
 <style>
 	.window {
 		position: absolute;
 		width: 300px;
+		user-select: none;
+		resize: both;
+		overflow: auto;
+		min-width: 20vw;
+		min-height: 10vh;
 	}
 	.title-bar-icon {
 		max-height: 12px;
+	}
+	.title-bar:active {
+		cursor: move;
 	}
 </style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
-	import { draggable } from '@neodrag/svelte';
 	import { processes } from '$lib/stores/processStore';
 </script>
 
 {#each $processes as { component, ...props }, i}
-	<div use:draggable>
+	<div>
 		<svelte:component this={component} {...props} />
 	</div>
 {/each}
